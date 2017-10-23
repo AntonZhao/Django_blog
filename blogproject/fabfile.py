5396be993eaa97821ff510574f615e671740303e
antonzhao <519102870@qq.com>
2017-10-23 16:59:47 +0800

    edit title

---
+++ blogproject/fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import env,run
+from fabric.operations import sudo
+
+GIT_REPO = "https://github.com/AntonZhao/Django_blog"
+
+env.user = 'AntonZhao'
+env.password = 'zx19950322'
+env.hosts = [39.106.48.224]
+env.port = '22'
+
+def deploy():
+    source_folder = '/home/anton/sites/antonzhao.cc/django_blog'
+    
+    run('cd %s && git pull' % source_folder)
+    run("""
+        cd{} &&
+        ../env/bin/pip install -r requirements.txt &&
+        ../env/bin/python3 manage.py collectstatic --noinput &&
+        ../env/bin/python3 manage.py migrate
+        """.format(source_folder)) 
+    sudo('restart gunicorn-demo.zmrenwu.com') ⑥
+    sudo('service nginx reload')	 
